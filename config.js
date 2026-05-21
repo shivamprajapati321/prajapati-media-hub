2f5c3f2735c929ef9beb835094890324fb2c1278
shivamprajapati321 <244211298+shivamprajapati321@users.noreply.github.com>
2026-05-21 12:12:09 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,1 @@
-window.PMH_CONFIG = {
-  COMPANY: "Prajapati Advertising",
-  UNIVERSAL_PASSWORD: "9922138138",
-  SUPABASE_URL: "https://tcilqogdbwnycvfiohjc.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY"
-};
+window.APP='PHASE4'
